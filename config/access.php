062556ac67a26cadb44cff11e551413bd1a8073b
elnisrmisr <elnisrmisr@gmail.com>
2026-04-12 22:43:39 +0200

    license3

--- config/access.php
+++ config/access.php
@@ -3,7 +3,7 @@
 return [
     'product' => env('APP_GUARD_PRODUCT', 'Elnisr'),
     'file' => env('APP_GUARD_FILE', storage_path('framework/.runtime_state')),
-    'public_key' => env('APP_GUARD_PUBLIC_KEY', 'lYjvutdiVxS44rhlsWFqnvHdFk5NiTVsLj0TCLpiFF4='),
+    'public_key' => env('APP_GUARD_PUBLIC_KEY', 'diZnKaw2OsHIg3e7Ih966PbjOclzA85fCG4PRv66hqM='),
     'state_prefix' => env('APP_GUARD_PREFIX', 'RTS-'),
     'accepted_prefixes' => [
         env('APP_GUARD_PREFIX', 'RTS-'),
